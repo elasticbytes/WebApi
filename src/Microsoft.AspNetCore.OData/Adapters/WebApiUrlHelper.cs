b44daf748d3f8f068b564f3cabab8ae1ef76b910
Rob Ward <robward@microsoft.com>
2017-11-09 10:20:48 -0800

    Initial port of AspNetCore. Add Adapters and Extensions for AspNetCore. Add placeholder classes for Formatting, Results and Routing. Refactor the UrlHelper to be seperate from the Request. Add per-route DI continers for AspNetCore, refactored per-route containers in AspNet.

--- src/Microsoft.AspNetCore.OData/Adapters/WebApiUrlHelper.cs
+++ src/Microsoft.AspNetCore.OData/Adapters/WebApiUrlHelper.cs
@@ -4,10 +4,17 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.AspNet.OData.Common;
+using Microsoft.AspNet.OData.Extensions;
 using Microsoft.AspNet.OData.Interfaces;
 using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OData.UriParser;
+using ODataPath = Microsoft.AspNet.OData.Routing.ODataPath;
 
 namespace Microsoft.AspNet.OData.Adapters
 {
@@ -17,32 +24,22 @@ namespace Microsoft.AspNet.OData.Adapters
     internal class WebApiUrlHelper : IWebApiUrlHelper
     {
         /// <summary>
-        /// The inner helper wrapped by this instance.
+        /// The inner request wrapped by this instance.
         /// </summary>
-        internal IUrlHelper innerHelper;
+        internal HttpRequest innerRequest;
 
         /// <summary>
         /// Initializes a new instance of the WebApiUrlHelper class.
         /// </summary>
         /// <param name="helper">The inner helper.</param>
-        public WebApiUrlHelper(IUrlHelper helper)
+        public WebApiUrlHelper(HttpRequest request)
         {
-            if (helper == null)
+            if (request == null)
             {
-                throw Error.ArgumentNull("helper");
+                throw Error.ArgumentNull("request");
             }
 
-            this.innerHelper = helper;
-        }
-
-        /// <summary>
-        /// Generates an OData link using the request's OData route name and path handler and given segments.
-        /// </summary>
-        /// <param name="segments">The OData path segments.</param>
-        /// <returns>The generated OData link.</returns>
-        public string CreateODataLink(IList<ODataPathSegment> segments)
-        {
-            throw new NotImplementedException();
+            this.innerRequest = request;
         }
 
         /// <summary>
@@ -52,7 +49,24 @@ namespace Microsoft.AspNet.OData.Adapters
         /// <returns>The generated OData link.</returns>
         public string CreateODataLink(params ODataPathSegment[] segments)
         {
-            throw new NotImplementedException();
+            return this.CreateODataLink(segments as IList<ODataPathSegment>);
+        }
+
+        /// <summary>
+        /// Generates an OData link using the request's OData route name and path handler and given segments.
+        /// </summary>
+        /// <param name="segments">The OData path segments.</param>
+        /// <returns>The generated OData link.</returns>
+        public string CreateODataLink(IList<ODataPathSegment> segments)
+        {
+            string routeName = this.innerRequest.ODataFeature().RouteName;
+            if (String.IsNullOrEmpty(routeName))
+            {
+                throw Error.InvalidOperation(SRResources.RequestMustHaveODataRouteName);
+            }
+
+            IODataPathHandler pathHandler = this.innerRequest.GetPathHandler();
+            return CreateODataLink(routeName, pathHandler, segments);
         }
 
         /// <summary>
@@ -64,7 +78,22 @@ namespace Microsoft.AspNet.OData.Adapters
         /// <returns>The generated OData link.</returns>
         public string CreateODataLink(string routeName, IODataPathHandler pathHandler, IList<ODataPathSegment> segments)
         {
-            throw new NotImplementedException();
+            if (String.IsNullOrEmpty(routeName))
+            {
+                throw Error.InvalidOperation(SRResources.RequestMustHaveODataRouteName);
+            }
+
+            if (pathHandler == null)
+            {
+                throw Error.ArgumentNull("pathHandler");
+            }
+
+            string odataPath = pathHandler.Link(new ODataPath(segments));
+
+            IUrlHelper urlHelper = this.innerRequest.HttpContext.GetUrlHelper();
+            return urlHelper.Link(
+                routeName,
+                new RouteValueDictionary() { { ODataRouteConstants.ODataPath, odataPath } });
         }
     }
 }
---
+++ src/Microsoft.AspNetCore.OData/PerRouteContainer.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Concurrent;
+using Microsoft.AspNet.OData.Common;
+
+namespace Microsoft.AspNet.OData
+{
+    internal class PerRouteContainer : PerRouteContainerBase
+    {
+        private ConcurrentDictionary<string, IServiceProvider> _perRouteContainers;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ODataSerializerContext"/> class.
+        /// </summary>
+        public PerRouteContainer()
+        {
+            this._perRouteContainers = new ConcurrentDictionary<string, IServiceProvider>();
+        }
+
+        /// <summary>
+        /// Get the root container for a given route name.
+        /// </summary>
+        /// <param name="routeName">The route name.</param>
+        /// <returns>The root container for the route name.</returns>
+        public override IServiceProvider GetODataRootContainer(string routeName)
+        {
+            IServiceProvider rootContainer;
+            if (_perRouteContainers.TryGetValue(routeName, out rootContainer))
+            {
+                return rootContainer;
+            }
+
+            throw Error.InvalidOperation(SRResources.NullContainer);
+        }
+
+        /// <summary>
+        /// Get the root container for a given route name.
+        /// </summary>
+        /// <param name="routeName">The route name.</param>
+        /// <param name="rootContainer">The root container to set.</param>
+        /// <returns>The root container for the route name.</returns>
+        /// <remarks>Used by unit tests to insert root containers.</remarks>
+        internal override void SetODataRootContainer(string routeName, IServiceProvider rootContainer)
+        {
+            if (rootContainer == null)
+            {
+                throw Error.InvalidOperation(SRResources.NullContainer);
+            }
+
+            this._perRouteContainers.AddOrUpdate(routeName, rootContainer, (k, v) => rootContainer);
+        }
+    }
+}
